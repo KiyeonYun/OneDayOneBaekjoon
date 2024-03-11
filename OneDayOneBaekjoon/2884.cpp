cc13ba9e39fb829b7d071eace8f8094f3613e5f1
KiyeonYun <kiyeon.yun@gmail.com>
2024-03-12 01:17:13 +0900

    Solved 2884

---
+++ OneDayOneBaekjoon/2884.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	int hour, minute;
+	const int CORRECTION = 45;
+	std::cin >> hour >> minute;
+
+	minute -= CORRECTION;
+
+	if (minute < 0) {
+		hour -= 1;
+		// 60 - minute가 0이 되기 위한 수
+		// => 60 + minute
+		minute += 60;
+		if (hour < 0) {
+			hour += 24;
+		}
+	}
+
+	std::cout << hour << " " << minute;
+}
