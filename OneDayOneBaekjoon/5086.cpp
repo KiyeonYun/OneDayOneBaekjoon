41c0d8396064313285c72483d0a4805a1f158d3a
KiyeonYun <kiyeon.yun@gmail.com>
2024-03-14 23:50:08 +0900

    Solved 5086

---
+++ OneDayOneBaekjoon/5086.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    while (0 != a && 0 != b) {
+        if (0 == b % a)
+            cout << "factor" << '\n';
+        else if (0 == a % b)
+            cout << "multiple" << '\n';
+        else
+            cout << "neither" << '\n';
+
+        cin >> a >> b;
+    }
+
+    return 0;
+}
